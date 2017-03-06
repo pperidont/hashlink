e32764dce2de40e8cf9e4da21a731dcdf3f42e02
Mathieu Capdegelle <mathieu.capdegelle@gmail.com>
2017-03-06 18:00:16 +0100

    openal ext begining

---
+++ libs/sdl/ALCImports.h
@@ -0,0 +1,28 @@
+// ----------------------------------------------------------------------------
+// ALC_EXT_thread_local_context
+// ----------------------------------------------------------------------------
+
+ALC_IMPORT(alcSetThreadContext, PFNALCSETTHREADCONTEXTPROC);
+ALC_IMPORT(alcGetThreadContext, PFNALCGETTHREADCONTEXTPROC);
+
+// ----------------------------------------------------------------------------
+// ALC_SOFT_loopback
+// ----------------------------------------------------------------------------
+
+ALC_IMPORT(alcLoopbackOpenDeviceSOFT, LPALCLOOPBACKOPENDEVICESOFT);
+ALC_IMPORT(alcIsRenderFormatSupportedSOFT, LPALCISRENDERFORMATSUPPORTEDSOFT);
+ALC_IMPORT(alcRenderSamplesSOFT, LPALCRENDERSAMPLESSOFT);
+
+// ----------------------------------------------------------------------------
+// ALC_SOFT_pause_device
+// ----------------------------------------------------------------------------
+
+ALC_IMPORT(alcDevicePauseSOFT, LPALCDEVICEPAUSESOFT);
+ALC_IMPORT(alcDeviceResumeSOFT, LPALCDEVICERESUMESOFT);
+
+// ----------------------------------------------------------------------------
+// ALC_SOFT_HRTF
+// ----------------------------------------------------------------------------
+
+ALC_IMPORT(alcGetStringiSOFT, LPALCGETSTRINGISOFT);
+ALC_IMPORT(alcResetDeviceSOFT, LPALCRESETDEVICESOFT);
--- libs/sdl/openal.c
+++ libs/sdl/openal.c
@@ -4,14 +4,20 @@
 #ifdef __APPLE__
 #include <OpenAL/al.h>
 #include <OpenAL/alc.h>
+#include <OpenAL/alext.h>
 #else
 #include <AL/al.h>
 #include <AL/alc.h>
+#include <AL/alext.h>
 #endif
 
 // ----------------------------------------------------------------------------
 // ALC
 // ----------------------------------------------------------------------------
+
+#define ALC_IMPORT(fun, t) t fun
+#include "ALCImports.h"
+#undef ALC_IMPORT
 
 // Context management
 
@@ -61,6 +67,11 @@ HL_PRIM int HL_NAME(alc_get_error)(ALCdevice *device) {
 
 // Extension support
 
+#define ALC_IMPORT(fun,t) fun = (t)alcGetProcAddress(device,#fun)
+HL_PRIM void HL_NAME(alc_load_extensions)(ALCdevice *device) {
+#	include "ALCImports.h"
+}
+
 HL_PRIM bool HL_NAME(alc_is_extension_present)(ALCdevice *device, vbyte *extname) {
 	return alcIsExtensionPresent(device, extname) == ALC_TRUE;
 }
@@ -68,8 +79,6 @@ HL_PRIM bool HL_NAME(alc_is_extension_present)(ALCdevice *device, vbyte *extname
 HL_PRIM int HL_NAME(alc_get_enum_value)(ALCdevice *device, vbyte *enumname) {
 	return alcGetEnumValue(device, enumname);
 }
-
-//HL_PRIM void* HL_NAME(alcGetProcAddress)(ALCdevice *device, const ALCchar *funcname);
 
 // Query function
 
@@ -119,6 +128,7 @@ DEFINE_PRIM(_BOOL,    alc_close_device, TDEVICE);
 
 DEFINE_PRIM(_I32,     alc_get_error, TDEVICE _NO_ARG);
 
+DEFINE_PRIM(_VOID,    alc_load_extensions,      TDEVICE);
 DEFINE_PRIM(_BOOL,    alc_is_extension_present, TDEVICE _BYTES);
 DEFINE_PRIM(_I32,     alc_get_enum_value,       TDEVICE _BYTES); 
 
@@ -134,6 +144,10 @@ DEFINE_PRIM(_VOID,    alc_capture_samples,      TDEVICE _BYTES _I32);
 // ----------------------------------------------------------------------------
 // AL
 // ----------------------------------------------------------------------------
+
+#define AL_IMPORT(fun, t) t fun
+#include "ALImports.h"
+#undef AL_IMPORT
 
 HL_PRIM void HL_NAME(al_doppler_factor)(float value) {
 	alDopplerFactor(value);
@@ -210,6 +224,12 @@ HL_PRIM int HL_NAME(al_get_error)() {
 }
 
 // Extension support
+
+#define AL_IMPORT(fun,t) fun = (t)alGetProcAddress(#fun)
+HL_PRIM void HL_NAME(al_load_extensions)() {
+#	include "ALImports.h"
+}
+
 
 HL_PRIM bool HL_NAME(al_is_extension_present)(vbyte *extname) {
 	return alIsExtensionPresent(extname) == AL_TRUE;
@@ -484,6 +504,7 @@ DEFINE_PRIM(_F32,   al_get_float,   _I32);
 DEFINE_PRIM(_F64,   al_get_double,  _I32);
 DEFINE_PRIM(_I32,   al_get_error,   _NO_ARG);
 
+DEFINE_PRIM(_VOID, al_load_extensions,      TDEVICE);
 DEFINE_PRIM(_BOOL, al_is_extension_present, _BYTES);
 DEFINE_PRIM(_I32,  al_get_enum_value,       _BYTES);
 
@@ -494,10 +515,10 @@ DEFINE_PRIM(_VOID, al_listeneri,  _I32 _I32);
 DEFINE_PRIM(_VOID, al_listener3i, _I32 _I32 _I32 _I32);
 DEFINE_PRIM(_VOID, al_listeneriv, _I32 _BYTES);
 
-DEFINE_PRIM(_F32, al_get_listenerf,  _I32);
+DEFINE_PRIM(_F32,  al_get_listenerf,  _I32);
 DEFINE_PRIM(_VOID, al_get_listener3f, _I32 _REF(_F32) _REF(_F32) _REF(_F32));
 DEFINE_PRIM(_VOID, al_get_listenerfv, _I32 _BYTES);
-DEFINE_PRIM(_I32, al_get_listeneri,  _I32);
+DEFINE_PRIM(_I32,  al_get_listeneri,  _I32);
 DEFINE_PRIM(_VOID, al_get_listener3i, _I32 _REF(_I32) _REF(_I32) _REF(_I32));
 DEFINE_PRIM(_VOID, al_get_listeneriv, _I32 _BYTES);
 
@@ -512,10 +533,10 @@ DEFINE_PRIM(_VOID, al_sourcei,  _I32 _I32 _I32);
 DEFINE_PRIM(_VOID, al_source3i, _I32 _I32 _I32 _I32 _I32);
 DEFINE_PRIM(_VOID, al_sourceiv, _I32 _I32 _BYTES);
 
-DEFINE_PRIM(_F32, al_get_sourcef,  _I32 _I32);
+DEFINE_PRIM(_F32,  al_get_sourcef,  _I32 _I32);
 DEFINE_PRIM(_VOID, al_get_source3f, _I32 _I32 _REF(_F32) _REF(_F32) _REF(_F32));
 DEFINE_PRIM(_VOID, al_get_sourcefv, _I32 _I32 _BYTES);
-DEFINE_PRIM(_I32, al_get_sourcei,  _I32 _I32);
+DEFINE_PRIM(_I32,  al_get_sourcei,  _I32 _I32);
 DEFINE_PRIM(_VOID, al_get_source3i, _I32 _I32 _REF(_I32) _REF(_I32) _REF(_I32));
 DEFINE_PRIM(_VOID, al_get_sourceiv, _I32 _I32 _BYTES);
 
@@ -543,9 +564,256 @@ DEFINE_PRIM(_VOID, al_bufferi,  _I32 _I32 _I32);
 DEFINE_PRIM(_VOID, al_buffer3i, _I32 _I32 _I32 _I32 _I32);
 DEFINE_PRIM(_VOID, al_bufferiv, _I32 _I32 _BYTES);
 
-DEFINE_PRIM(_F32, al_get_bufferf,  _I32 _I32);
+DEFINE_PRIM(_F32,  al_get_bufferf,  _I32 _I32);
 DEFINE_PRIM(_VOID, al_get_buffer3f, _I32 _I32 _REF(_F32) _REF(_F32) _REF(_F32));
 DEFINE_PRIM(_VOID, al_get_bufferfv, _I32 _I32 _BYTES);
-DEFINE_PRIM(_I32, al_get_bufferi,  _I32 _I32);
+DEFINE_PRIM(_I32,  al_get_bufferi,  _I32 _I32);
 DEFINE_PRIM(_VOID, al_get_buffer3i, _I32 _I32 _REF(_I32) _REF(_I32) _REF(_I32));
 DEFINE_PRIM(_VOID, al_get_bufferiv, _I32 _I32 _BYTES);
+
+#define CHECK_EXT(fun) if(fun == NULL) hl_error("Unsupported extension function")
+
+// ----------------------------------------------------------------------------
+// EXT_thread_local_context Extension
+// ----------------------------------------------------------------------------
+
+HL_PRIM bool HL_NAME(alc_set_thread_context)(ALCcontext *context) {
+	CHECK_EXT(alcSetThreadContext);
+	return alcSetThreadContext(context);
+}
+
+HL_PRIM ALCcontext* HL_NAME(alc_get_thread_context)() {
+	CHECK_EXT(alcGetThreadContext);
+	return alcGetThreadContext();
+}
+
+// ----------------------------------------------------------------------------
+// ALC_SOFT_loopback Extension
+// ----------------------------------------------------------------------------
+
+HL_PRIM ALCdevice* HL_NAME(alc_open_device_soft)(vbyte *devicename) {
+	CHECK_EXT(alcLoopbackOpenDeviceSOFT);
+	return alcLoopbackOpenDeviceSOFT(devicename);
+}
+
+HL_PRIM bool HL_NAME(alc_is_render_format_supported_soft)(ALCdevice *device, int freq, int channels, int type) {
+	CHECK_EXT(alcIsRenderFormatSupportedSOFT);
+	return alcIsRenderFormatSupportedSOFT(device, freq, channels, type);
+}
+
+HL_PRIM void HL_NAME(alc_render_samples_soft)(ALCdevice *device, vbyte *buffer, int samples) {
+	CHECK_EXT(alcRenderSamplesSOFT);
+	alcRenderSamplesSOFT(device, buffer, samples);
+}
+
+// ----------------------------------------------------------------------------
+// ALC_SOFT_pause_device Extension
+// ----------------------------------------------------------------------------
+
+HL_PRIM void HL_NAME(alc_device_pause_soft)(ALCdevice *device) {
+	CHECK_EXT(alcDevicePauseSOFT);
+	alcDevicePauseSOFT(device);
+}
+
+HL_PRIM void HL_NAME(alc_device_resume_soft)(ALCdevice *device) {
+	CHECK_EXT(alcDeviceResumeSOFT);
+	alcDeviceResumeSOFT(device);
+}
+
+// ----------------------------------------------------------------------------
+// SOFT_HRTF Extension
+// ----------------------------------------------------------------------------
+
+HL_PRIM vbyte* HL_NAME(alc_get_stringi_soft)(ALCdevice *device, int param, int index) {
+	CHECK_EXT(alcGetStringiSOFT);
+	return (vbyte*)alcGetStringiSOFT(device, param, index);
+}
+
+HL_PRIM bool HL_NAME(alc_reset_device_soft)(ALCdevice *device, vbyte *attribs) {
+	CHECK_EXT(alcResetDeviceSOFT);
+	return alcResetDeviceSOFT(device, (ALCint*)attribs);
+}
+
+// ----------------------------------------------------------------------------
+// AL_EXT_STATIC_BUFFER Extension
+// ----------------------------------------------------------------------------
+
+HL_PRIM void HL_NAME(al_buffer_data_static)(unsigned buffer, int format, vbyte *data, int len, int freq) {
+	CHECK_EXT(alBufferDataStatic);
+	alBufferDataStatic(buffer, format, data, len, freq);
+}
+
+// ----------------------------------------------------------------------------
+// AL_SOFT_buffer_sub_data Extension
+// ----------------------------------------------------------------------------
+
+HL_PRIM void HL_NAME(al_buffer_sub_data_soft)(unsigned buffer, int format, vbyte *data, int offset, int length) {
+	CHECK_EXT(alBufferSubDataSOFT);
+	alBufferSubDataSOFT(buffer, format, data, offset, length);
+}
+
+// ----------------------------------------------------------------------------
+// AL_EXT_FOLDBACK Extension
+// ----------------------------------------------------------------------------
+
+HL_PRIM void HL_NAME(al_request_foldback_start)(int mode, int count, int length, vbyte *mem, vclosure *callback) {
+	CHECK_EXT(alRequestFoldbackStart);
+	if (callback->hasValue) if( callback->hasValue ) hl_error("Cannot set foldback on closure callback");
+	alRequestFoldbackStart(mode, count, length, (ALfloat*)mem, (LPALFOLDBACKCALLBACK)callback->fun);
+}
+
+HL_PRIM void HL_NAME(al_request_foldback_stop)() {
+	CHECK_EXT(alRequestFoldbackStop);
+	alRequestFoldbackStop();
+}
+
+// ----------------------------------------------------------------------------
+// AL_SOFT_buffer_samples Extension
+// ----------------------------------------------------------------------------
+
+HL_PRIM void HL_NAME(al_buffer_samples_soft)(unsigned buffer, int samplerate, int internatlformat, int samples, int channels, int type, vbyte *data) {
+	CHECK_EXT(alBufferSamplesSOFT);
+	alBufferSamplesSOFT(buffer, samplerate, internatlformat, samples, channels, type, data);
+}
+
+HL_PRIM void HL_NAME(al_buffer_sub_samples_soft)(unsigned buffer, int offset, int samples, int channels, int type, vbyte *data) {
+	CHECK_EXT(alBufferSubSamplesSOFT);
+	alBufferSubSamplesSOFT(buffer, offset, samples, channels, type, data);
+}
+
+HL_PRIM void HL_NAME(al_get_buffer_samples_soft)(unsigned buffer, int offset, int samples, int channels, int type, vbyte *data) {
+	CHECK_EXT(alGetBufferSamplesSOFT);
+	alGetBufferSamplesSOFT(buffer, offset, samples, channels, type, data);
+}
+
+HL_PRIM bool HL_NAME(al_is_buffer_format_supported_soft)(int format) {
+	CHECK_EXT(alIsBufferFormatSupportedSOFT);
+	return alIsBufferFormatSupportedSOFT(format);
+}
+
+// ----------------------------------------------------------------------------
+// AL_SOFT_source_latency Extension
+// ----------------------------------------------------------------------------
+
+HL_PRIM void HL_NAME(al_sourced_soft)(unsigned source, int param, double value) {
+	CHECK_EXT(alSourcedSOFT);
+	alSourcedSOFT(source, param, value);
+}
+
+HL_PRIM void HL_NAME(al_source3d_soft)(unsigned source, int param, double value1, double value2, double value3) {
+	CHECK_EXT(alSource3dSOFT);
+	alSource3dSOFT(source, param, value1, value2, value3);
+}
+
+HL_PRIM void HL_NAME(al_sourcedv_soft)(unsigned source, int param, vbyte *values) {
+	CHECK_EXT(alSourcedvSOFT);
+	alSourcedvSOFT(source, param, (ALdouble*)values);
+}
+
+HL_PRIM double HL_NAME(al_get_sourced_soft)(unsigned source, int param) {
+	double value;
+	CHECK_EXT(alGetSourcedSOFT);
+	alGetSourcedSOFT(source, param, &value);
+	return value;
+}
+
+HL_PRIM void HL_NAME(al_get_source3d_soft)(unsigned source, int param, double *value1, double *value2, double *value3) {
+	CHECK_EXT(alGetSource3dSOFT);
+	alGetSource3dSOFT(source, param, value1, value2, value3);
+}
+
+HL_PRIM void HL_NAME(al_get_sourcedv_soft)(unsigned source, int param, vbyte *values) {
+	CHECK_EXT(alGetSourcedvSOFT);
+	alGetSourcedvSOFT(source, param, (ALdouble*)values);
+}
+
+#define I64_COMBINE(hi, lo) (((ALint64SOFT)hi) << 32) | lo
+
+HL_PRIM void HL_NAME(al_sourcei64_soft)(unsigned source, int param, int valueHi, int valueLo) {
+	CHECK_EXT(alSourcei64SOFT);
+	alSourcei64SOFT(source, param, I64_COMBINE(valueHi, valueLo));
+}
+
+HL_PRIM void HL_NAME(al_source3i64_soft)(unsigned source, int param, 
+		int value1Hi, int value1Lo, int value2Hi, int value2Lo, int value3Hi, int value3Lo) {
+	CHECK_EXT(alSource3i64SOFT);
+	alSource3i64SOFT(source, param, 
+		I64_COMBINE(value1Hi, value1Lo), 
+		I64_COMBINE(value2Hi, value2Lo), 
+		I64_COMBINE(value3Hi, value3Lo));
+}
+
+HL_PRIM void HL_NAME(al_sourcei64v_soft)(unsigned source, int param, vbyte *values) {
+	CHECK_EXT(alSourcei64vSOFT);
+	alSourcei64vSOFT(source, param, (ALint64SOFT*)values);
+}
+
+#define I64_HI(v) v >> 32
+#define I64_LO(v) v & 0xFFFFFFFF;
+
+HL_PRIM void HL_NAME(al_get_sourcei64_soft)(unsigned source, int param, int *valueHi, int *valueLo) {
+	ALint64SOFT value;
+	CHECK_EXT(alGetSourcei64SOFT);
+	alGetSourcei64SOFT(source, param, &value);
+	*valueHi = I64_HI(value);
+	*valueLo = I64_LO(value);
+}
+
+HL_PRIM void HL_NAME(al_get_source3i64_soft)(unsigned source, int param, 
+		int *value1Hi, int *value1Lo, int *value2Hi, int *value2Lo, int *value3Hi, int *value3Lo) {
+	ALint64SOFT value1, value2, value3;
+	CHECK_EXT(alGetSource3i64SOFT);
+	alGetSource3i64SOFT(source, param, &value1, &value2, &value3);
+	*value1Hi = I64_HI(value1);
+	*value1Lo = I64_LO(value1);
+	*value2Hi = I64_HI(value2);
+	*value2Lo = I64_LO(value2);
+	*value3Hi = I64_HI(value3);
+	*value3Lo = I64_LO(value3);
+}
+
+HL_PRIM void HL_NAME(al_get_sourcei64v_soft)(unsigned source, int param, vbyte *values) {
+	CHECK_EXT(alGetSourcei64vSOFT);
+	alGetSourcei64vSOFT(source, param, (ALint64SOFT*)values);
+}
+
+// ----------------------------------------------------------------------------
+// AL_SOFT_deferred_updates
+// ----------------------------------------------------------------------------
+
+HL_PRIM void HL_NAME(al_defer_updates_soft)() {
+	CHECK_EXT(alDeferUpdatesSOFT);
+	alDeferUpdatesSOFT();
+}
+
+HL_PRIM void HL_NAME(al_process_updates_soft)() {
+	CHECK_EXT(alProcessUpdatesSOFT);
+	alProcessUpdatesSOFT();
+}
+
+//AL_API void AL_APIENTRY alRequestFoldbackStart(ALenum mode,ALsizei count,ALsizei length,ALfloat *mem,LPALFOLDBACKCALLBACK callback);
+//AL_API void AL_APIENTRY alRequestFoldbackStop(void);
+
+//AL_API void AL_APIENTRY alBufferSamplesSOFT(ALuint buffer, ALuint samplerate, ALenum internalformat, ALsizei samples, ALenum channels, ALenum type, const ALvoid *data);
+
+//AL_API void AL_APIENTRY alBufferSubSamplesSOFT(ALuint buffer, ALsizei offset, ALsizei samples, ALenum channels, ALenum type, const ALvoid *data);
+//AL_API void AL_APIENTRY alGetBufferSamplesSOFT(ALuint buffer, ALsizei offset, ALsizei samples, ALenum channels, ALenum type, ALvoid *data);
+//AL_API ALboolean AL_APIENTRY alIsBufferFormatSupportedSOFT(ALenum format);
+
+//AL_API void AL_APIENTRY alSourcedSOFT(ALuint source, ALenum param, ALdouble value);
+//AL_API void AL_APIENTRY alSource3dSOFT(ALuint source, ALenum param, ALdouble value1, ALdouble value2, ALdouble value3);
+//AL_API void AL_APIENTRY alSourcedvSOFT(ALuint source, ALenum param, const ALdouble *values);
+//AL_API void AL_APIENTRY alGetSourcedSOFT(ALuint source, ALenum param, ALdouble *value);
+//AL_API void AL_APIENTRY alGetSource3dSOFT(ALuint source, ALenum param, ALdouble *value1, ALdouble *value2, ALdouble *value3);
+//AL_API void AL_APIENTRY alGetSourcedvSOFT(ALuint source, ALenum param, ALdouble *values);
+//AL_API void AL_APIENTRY alSourcei64SOFT(ALuint source, ALenum param, ALint64SOFT value);
+
+//AL_API void AL_APIENTRY alSource3i64SOFT(ALuint source, ALenum param, ALint64SOFT value1, ALint64SOFT value2, ALint64SOFT value3);
+//AL_API void AL_APIENTRY alSourcei64vSOFT(ALuint source, ALenum param, const ALint64SOFT *values);
+
+//AL_API void AL_APIENTRY alGetSourcei64SOFT(ALuint source, ALenum param, ALint64SOFT *value);
+//AL_API void AL_APIENTRY alGetSource3i64SOFT(ALuint source, ALenum param, ALint64SOFT *value1, ALint64SOFT *value2, ALint64SOFT *value3);
+//AL_API void AL_APIENTRY alGetSourcei64vSOFT(ALuint source, ALenum param, ALint64SOFT *values);
+
+//AL_API ALvoid AL_APIENTRY alDeferUpdatesSOFT(void);
+//AL_API ALvoid AL_APIENTRY alProcessUpdatesSOFT(void);
