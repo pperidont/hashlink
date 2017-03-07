fe11e7ded8ca19a9c3f294a78a8e9cc7e8a8f5eb
Pascal Peridont <pperidont@motion-twin.com>
2017-03-07 09:59:34 +0100

    Fix ALC compilation

--- libs/sdl/openal/ALC.hx
+++ libs/sdl/openal/ALC.hx
@@ -22,7 +22,7 @@ extern class ALC {
 	public static function getError (device : Device) : Int;
 
 	// Extension support
-	public static function loadExtensions     (device : Device);
+	public static function loadExtensions     (device : Device) : Void;
 	public static function isExtensionPresent (device : Device, extname : hl.Bytes)  : Bool;
 	public static function getEnumValue       (device : Device, enumname : hl.Bytes) : Int;
 
@@ -130,9 +130,9 @@ extern class ALC {
 	public static inline var _7POINT1_SOFT                    = 0x1506;
 	
 
-	public static function loopbackOpenDeviceSoft      (devicename : hl.Bytes) : Device;
-	public static function isRenderFormatSupportedSoft (device : Device, freq : Int, channels : Int, type : Int);
-	public static function renderSamplesSoft           (device : Device, buffer : hl.Bytes, samples : Int);
+	public static function openDeviceSoft              (devicename : hl.Bytes) : Device;
+	public static function isRenderFormatSupportedSoft (device : Device, freq : Int, channels : Int, type : Int) : Bool;
+	public static function renderSamplesSoft           (device : Device, buffer : hl.Bytes, samples : Int) : Void;
 
 	// ----------------------------------------------------------------------------
 	// ALC_EXT_DEFAULT_FILTER_ORDER
@@ -144,25 +144,25 @@ extern class ALC {
 	// ALC_SOFT_pause_device
 	// ----------------------------------------------------------------------------
 
-	public static function devicePauseSoft  (device : Device);
-	public static function deviceResumeSoft (device : Device);
+	public static function devicePauseSoft  (device : Device) : Void;
+	public static function deviceResumeSoft (device : Device) : Void;
 
 	// ----------------------------------------------------------------------------
 	// ALC_SOFT_HRTF
 	// ----------------------------------------------------------------------------
 
-	public static inline var HRTF_SOFT                        = 0x1992
-	public static inline var DONT_CARE_SOFT                   = 0x0002
-	public static inline var HRTF_STATUS_SOFT                 = 0x1993
-	public static inline var HRTF_DISABLED_SOFT               = 0x0000
-	public static inline var HRTF_ENABLED_SOFT                = 0x0001
-	public static inline var HRTF_DENIED_SOFT                 = 0x0002
-	public static inline var HRTF_REQUIRED_SOFT               = 0x0003
-	public static inline var HRTF_HEADPHONES_DETECTED_SOFT    = 0x0004
-	public static inline var HRTF_UNSUPPORTED_FORMAT_SOFT     = 0x0005
-	public static inline var NUM_HRTF_SPECIFIERS_SOFT         = 0x1994
-	public static inline var HRTF_SPECIFIER_SOFT              = 0x1995
-	public static inline var HRTF_ID_SOFT                     = 0x1996
+	public static inline var HRTF_SOFT                        = 0x1992;
+	public static inline var DONT_CARE_SOFT                   = 0x0002;
+	public static inline var HRTF_STATUS_SOFT                 = 0x1993;
+	public static inline var HRTF_DISABLED_SOFT               = 0x0000;
+	public static inline var HRTF_ENABLED_SOFT                = 0x0001;
+	public static inline var HRTF_DENIED_SOFT                 = 0x0002;
+	public static inline var HRTF_REQUIRED_SOFT               = 0x0003;
+	public static inline var HRTF_HEADPHONES_DETECTED_SOFT    = 0x0004;
+	public static inline var HRTF_UNSUPPORTED_FORMAT_SOFT     = 0x0005;
+	public static inline var NUM_HRTF_SPECIFIERS_SOFT         = 0x1994;
+	public static inline var HRTF_SPECIFIER_SOFT              = 0x1995;
+	public static inline var HRTF_ID_SOFT                     = 0x1996;
 
 	public static function getStringiSoft  (device : Device, param : Int, index : Int) : hl.Bytes;
 	public static function resetDeviceSoft (device : Device, attribs : hl.Bytes) : Bool;
